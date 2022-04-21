0e8b3e11ed08a3bb588db99dee5ed0583a72dbff
Daniel Doyle <daniel.doyle@outlook.ie>
2022-04-21 17:45:56 +0100

    CV bug

--- src/components/CV/CV.js
+++ src/components/CV/CV.js
@@ -4,6 +4,6 @@ import './CV.css';
 export default function CV() {
 
     return (
-        <object data="/media/CV/CV.pdf" type="application/pdf" className='cv-object' width="100%" height="100%"></object>
+        <object data="/media/CV/CV.pdf" type="application/pdf" className='cv-object' width="700px" height="1000px"></object>
     )
 }
