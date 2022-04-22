5547a673940f938cbe69b9c43ea0afd06621bf9e
Daniel Doyle <daniel.doyle@outlook.ie>
2022-04-22 10:22:11 +0100

    CV fix

--- src/components/CV/CV.js
+++ src/components/CV/CV.js
@@ -7,6 +7,7 @@ export default function CV() {
         <div className='cv'>
             <img src="media/CV/CV_1.jpg" alt='resume picture 1'/>
             <img src="media/CV/CV_2.jpg" alt='resume picture 2'/>
+            
         </div>
     )
 };
